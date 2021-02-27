af9e086a7dbc4f1d816f3da0604aef2379856dc9
Melissa Hall <mhall2782@gmail.com>
2021-02-27 15:39:54 -0500

    repl challenges updates

--- 1.2-arrays-objects.instructions.js
+++ 1.2-arrays-objects.instructions.js
@@ -6,36 +6,18 @@
     - use console.log statements to show that you can print out the nameOfMovie, runTime, the characters, the name of a character, and one of the items a character has. Your console should look similar to the screenshot below.
 */
 
-//NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
-
-
 let movies = new Object();
 
 console.log(movies)
-//{}
 
 movies.name = "Boyz N the Hood";
-movies.releaseYear = "1991";
+movies.releaseYear = 1991;
 movies.genre = ["Crime", "Drama"];
+movies.characters = ["Tre Styles", "Doughboy"];
 movies.ratings = {
   IMDb: "7.8 / 10",
   Metacritic: "76%"
+
 };
 
-
-let characters = ["Tre Syles", "Doughboy"];
-let char1 = {
-    firstName: "Tre",
-    lastName: "Styles",
-    age: 18,
-    school: "Crenshaw",
-  };
-
-  var person = {
-    firstName: "Tre",
-    lastName: "Styles"
-  };
-  
-  alert(
-  person.Tre
-  );
+console.log(movies);
